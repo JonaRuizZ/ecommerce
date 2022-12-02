f88c486fa461f48f577cb9630e78b2826f1ff6a7
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2022-12-02 14:00:09 -0300

    Se modifica parrafo para mostrar rama dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { API_URL } from "../env";
 
 const App = () => {
   return (
-    <h1>Bienvenidos {API_URL}</h1>
-  )};
+    <>
+      <h1>Bienvenidos {API_URL}</h1>
+      <p>Estos cambios están hechos por branch dev</p>
+    </>
+  )
+};
 
 export default App;
