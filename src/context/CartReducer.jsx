aca634ea695d56fbe2df055010780ab310bc0cf1
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2023-01-05 01:13:21 -0300

    Se le dio funcionalidad a los botones de agregar y quitar carrito

--- src/context/CartReducer.jsx
+++ src/context/CartReducer.jsx
@@ -1,5 +1,12 @@
 const CartReducer = (state, action) => {
     // console.log(action) para ver la accion cuando tengamos algun problema
+    console.log(action)
+    // action: { es un objeto que contiene: 
+    //    - payload: Contiene los datos del producto de la API
+    //    - type: Contiene el case del switch como por ejemplo: "REMOVE_FROM_CART", "ADD_TO_CART" etc...
+    // }
+    console.log(state)
+    // state: es el {cart: Array(4)} cuantos productos hemos ingresado al carrito
     switch (action.type) {
         case "ADD_TO_CART":
             return { 
@@ -8,11 +15,11 @@ const CartReducer = (state, action) => {
             }
         case "REMOVE_FROM_CART":
             return { 
-                ...state, 
-                cart:[...state.cart.filter(p => p.id === action.payload.id)],
+                ...state,
+                cart:[...state.cart.filter(p => p.id !== action.payload.id)]
             }
         case "CLEAR_CART":
-            return{
+            return {
                 cart:[]
             }
         default:
